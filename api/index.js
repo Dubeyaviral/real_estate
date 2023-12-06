b00a79be30d37117301c3b53a2565be2bd5ce753
Aviral <aviraldubey2000@gmail.com>
2023-12-06 15:16:13 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config()
 
@@ -16,8 +17,14 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 })
 
 const app = express();
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
